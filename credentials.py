e1edd5d27b6dd328a124196d2471bee1df576296
Chase Kregor <chasekregor@hey.com>
2021-01-30 08:51:39 -0700

    Moved credentials.py location

--- credentials.py
+++ credentials.py
@@ -1,3 +1,4 @@
 #https://www.strava.com/settings/api
+#uncomment variables below to your own Strava API Application
 client_id = #239749
 client_secret = #'jfsdlkdjf8ds9f78dsf7sdf97ds89f7dsfds987fds98'
